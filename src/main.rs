222846b43ea948f04ec0be762c74d7266fa974d1
Martin Gonzalez Perna <mgonzalezperna@gmail.com>
2024-04-25 22:26:01 +0200

    add client spawner

--- src/main.rs
+++ src/main.rs
@@ -75,3 +75,43 @@ async fn spawn_sever(addr: String) {
         tokio::spawn(handle_connection(state.clone(), stream, addr));
     }
 }
+
+async fn spawn_client(connect_addr: String) {
+    let url = url::Url::parse(&connect_addr).expect("Problem parsing the URL.");
+
+    let (stdin_tx, stdin_rx) = futures_channel::mpsc::unbounded();
+    tokio::spawn(read_stdin(stdin_tx));
+
+    dbg!(&url);
+
+    let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
+    println!("WebSocket handshake has been successfully completed");
+
+    let (write, read) = ws_stream.split();
+
+    let stdin_to_ws = stdin_rx.map(Ok).forward(write);
+    let ws_to_stdout = {
+        read.for_each(|message| async {
+            let data = message.unwrap().into_data();
+            tokio::io::stdout().write_all(&data).await.unwrap();
+        })
+    };
+
+    pin_mut!(stdin_to_ws, ws_to_stdout);
+    future::select(stdin_to_ws, ws_to_stdout).await;
+}
+
+// Our helper method which will read data from stdin and send it along the
+// sender provided.
+async fn read_stdin(tx: futures_channel::mpsc::UnboundedSender<Message>) {
+    let mut stdin = tokio::io::stdin();
+    loop {
+        let mut buf = vec![0; 1024];
+        let n = match stdin.read(&mut buf).await {
+            Err(_) | Ok(0) => break,
+            Ok(n) => n,
+        };
+        buf.truncate(n);
+        tx.unbounded_send(Message::binary(buf)).unwrap();
+    }
+}
