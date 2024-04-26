731efd99707621cf55e6f49607c2d4fb0a7f8f63
Tincho <31832018+mgonzalezperna@users.noreply.github.com>
2024-04-26 23:34:34 +0200

    Websocket PoC: add feature to create clients (#2)
    Now doing `cargo run` launches a prompt requesting to select if the user
    wants to create a Server or a Client, improving usability.
    Creating a Client if Server doesn't exist crashes the app.
    
    Changelog:
    * move spawn server into its own fn, improve cohesivity
    * add client spawner
    * refactor main to allow server or client creation
    * add inquire prompt
    * add new dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 use std::{
     collections::HashMap,
     env,
-    io::Error as IoError,
+    fmt::Display,
     net::SocketAddr,
     sync::{Arc, Mutex},
 };
 
 use futures_channel::mpsc::{unbounded, UnboundedSender};
 use futures_util::{future, pin_mut, stream::TryStreamExt, StreamExt};
-
+use inquire::{InquireError, Select};
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
-use tokio_tungstenite::tungstenite::protocol::Message;
+use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
 
 type Tx = UnboundedSender<Message>;
 type PeerMap = Arc<Mutex<HashMap<SocketAddr, Tx>>>;
@@ -30,12 +33,18 @@ async fn handle_connection(peer_map: PeerMap, raw_stream: TcpStream, addr: Socke
     let (outgoing, incoming) = ws_stream.split();
 
     let broadcast_incoming = incoming.try_for_each(|msg| {
-        println!("Received a message from {}: {}", addr, msg.to_text().unwrap());
+        println!(
+            "Received a message from {}: {}",
+            addr,
+            msg.to_text().unwrap()
+        );
         let peers = peer_map.lock().unwrap();
 
         // We want to broadcast the message to everyone except ourselves.
-        let broadcast_recipients =
-            peers.iter().filter(|(peer_addr, _)| peer_addr != &&addr).map(|(_, ws_sink)| ws_sink);
+        let broadcast_recipients = peers
+            .iter()
+            .filter(|(peer_addr, _)| peer_addr != &&addr)
+            .map(|(_, ws_sink)| ws_sink);
 
         for recp in broadcast_recipients {
             recp.unbounded_send(msg.clone()).unwrap();
@@ -53,10 +62,7 @@ async fn handle_connection(peer_map: PeerMap, raw_stream: TcpStream, addr: Socke
     peer_map.lock().unwrap().remove(&addr);
 }
 
-#[tokio::main]
-async fn main() -> Result<(), IoError> {
-    let addr = env::args().nth(1).unwrap_or_else(|| "127.0.0.1:8080".to_string());
-
+async fn spawn_sever(addr: String) {
     let state = PeerMap::new(Mutex::new(HashMap::new()));
 
     // Create the event loop and TCP listener we'll accept connections on.
@@ -68,6 +74,77 @@ async fn main() -> Result<(), IoError> {
     while let Ok((stream, addr)) = listener.accept().await {
         tokio::spawn(handle_connection(state.clone(), stream, addr));
     }
+}
 
-    Ok(())
+async fn spawn_client(connect_addr: String) {
+    let url = url::Url::parse(&connect_addr).expect("Problem parsing the URL.");
+
+    let (stdin_tx, stdin_rx) = futures_channel::mpsc::unbounded();
+    tokio::spawn(read_stdin(stdin_tx));
+
+    dbg!(&url);
+
+    let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
+    println!("WebSocket handshake has been successfully completed");
+
+    let (write, read) = ws_stream.split();
+
+    let stdin_to_ws = stdin_rx.map(Ok).forward(write);
+    let ws_to_stdout = {
+        read.for_each(|message| async {
+            let data = message.unwrap().into_data();
+            tokio::io::stdout().write_all(&data).await.unwrap();
+        })
+    };
+
+    pin_mut!(stdin_to_ws, ws_to_stdout);
+    future::select(stdin_to_ws, ws_to_stdout).await;
+}
+
+// Our helper method which will read data from stdin and send it along the
+// sender provided.
+async fn read_stdin(tx: futures_channel::mpsc::UnboundedSender<Message>) {
+    let mut stdin = tokio::io::stdin();
+    loop {
+        let mut buf = vec![0; 1024];
+        let n = match stdin.read(&mut buf).await {
+            Err(_) | Ok(0) => break,
+            Ok(n) => n,
+        };
+        buf.truncate(n);
+        tx.unbounded_send(Message::binary(buf)).unwrap();
+    }
+}
+
+#[derive(Debug, EnumIter)]
+enum Option {
+    Server,
+    Client,
+}
+
+impl Display for Option {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let state = match self {
+            Option::Server => "Server",
+            Option::Client => "Client",
+        };
+        write!(f, "{}", state)
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let options: Vec<_> = Option::iter().collect();
+    let addr = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "localhost:8080".to_string());
+
+    let ans: Result<Option, InquireError> =
+        Select::new("What's your favorite fruit?", options).prompt();
+
+    match ans {
+        Ok(Option::Server) => spawn_sever(addr).await,
+        Ok(Option::Client) => spawn_client("ws://".to_string() + &addr).await,
+        Err(_) => panic!("Error has ocurred."),
+    }
 }
