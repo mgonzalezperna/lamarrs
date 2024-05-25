cd8f8f585c5c769fb9665801aa9bd68e27aea9ee
Martin Gonzalez Perna <mgonzalezperna@gmail.com>
2024-05-25 18:57:16 +0200

    remove unused functions, import modules

--- lamarrs-utils/src/lib.rs
+++ lamarrs-utils/src/lib.rs
@@ -1,17 +1,8 @@
-use futures_channel::mpsc::{unbounded, UnboundedSender};
-use futures_util::{future, pin_mut, stream::TryStreamExt, StreamExt};
-use std::{
-    collections::HashMap,
-    net::SocketAddr,
-    sync::{Arc, Mutex},
-};
-use tokio::io::AsyncReadExt;
-use tokio::net::TcpStream;
-use tokio_tungstenite::tungstenite::protocol::Message;
 pub mod error;
+pub mod enums;
+pub mod messages;
 
-pub type PeerMap = Arc<Mutex<HashMap<SocketAddr, Tx>>>;
-type Tx = UnboundedSender<Message>;
+use tokio::io::AsyncReadExt;
 
 // Our helper method which will read data from stdin and send it along the
 // sender provided.
@@ -26,20 +17,3 @@ pub async fn read_stdin() -> Vec<u8> {
     buf
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    struct FakeClient {
-        // Receiver from websocket connection
-        read: mpsc::Receiver<ServerResponse>,
-        // Sender to websocket connection
-        write: mpsc::Sender<ServerMessage>,
-    }
-
-    #[test]
-    fn it_works() {
-        let result = 4;
-        assert_eq!(result, 4);
-    }
-}
