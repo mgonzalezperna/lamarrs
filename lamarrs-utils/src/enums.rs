994b3ff605e7e982807edf7be53d1a31edc5cec9
Martin Gonzalez Perna <mgonzalezperna@gmail.com>
2024-06-10 22:21:34 +0200

    rework Messages to adapt to new subscriber actor

--- lamarrs-utils/src/enums.rs
+++ lamarrs-utils/src/enums.rs
@@ -1,19 +1,49 @@
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 use crate::messages::*;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum MessageError{
+    #[error("Error guessing the Message type")]
+    GuessingError(String),
+    #[error("Failed to find Message Type: {0}")]
+    SerializationError(#[from] serde_json::Error),
+}
 
 #[derive(Deserialize, Serialize, PartialEq, Debug, Clone)]
 pub enum GatewayMessage{
+    RegisterResult(RegisterResult),
+    Suscribe(Suscribe),
     SubscribeResult(SubscribeResult),
     Subtitle(Subtitle),
-    CloseConnection(CloseConnectionReason)
+    Color(Color),
+    Error(GatewayError)
 }
 
 impl std::fmt::Display for GatewayMessage{
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let value = match self {
+            Self::RegisterResult(inner) => serde_json::to_value(inner).unwrap(),
+            Self::Suscribe(inner) => serde_json::to_value(inner).unwrap(),
             Self::SubscribeResult(inner) => serde_json::to_value(inner).unwrap(),
             Self::Subtitle(inner) => serde_json::to_value(inner).unwrap(),
-            Self::CloseConnection(inner)=> serde_json::to_value(inner).unwrap(),
+            Self::Color(inner) => serde_json::to_value(inner).unwrap(),
+            Self::Error(inner) => serde_json::to_value(inner).unwrap(),
+        };
+        write!(f, "{}", value)
+    }
+}
+
+#[derive(Deserialize, Serialize, PartialEq, Debug, Clone)]
+pub enum GatewayError{
+    UnregisteredSubscriber,
+}
+
+impl std::fmt::Display for GatewayError{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let value = match self {
+            Self::UnregisteredSubscriber => "UnregisteredSubscriber"
         };
         write!(f, "{}", value)
     }
@@ -21,23 +51,31 @@ impl std::fmt::Display for GatewayMessage{
 
 #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
 pub enum SubscriberMessage{
+    // The first message after connecting must be a Register, with its own UUID.
+    Register((Uuid, RelativeLocation)),
     // A Client wants to subscribe to the service.
-    Subscribe(Subscribe),
+    Subscribe(Service),
     // A Client wants to update its location.
-    Location(Location),
     CloseConnection(CloseConnectionReason),
 }
 
 impl std::fmt::Display for SubscriberMessage{
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let value = match self {
+            Self::Register(inner) => serde_json::to_value(inner).unwrap(),
             Self::Subscribe(inner) => serde_json::to_value(inner).unwrap(),
-            Self::Location(inner) => serde_json::to_value(inner).unwrap(),
             Self::CloseConnection(inner)=> serde_json::to_value(inner).unwrap(),
         };
         write!(f, "{}", value)
     }
 }
+
+impl SubscriberMessage {
+    pub fn deserialize(data: String) -> Option<Self> {
+        serde_json::from_str(&data).ok()
+    } 
+}
+
 
 #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
 pub enum RelativeLocation{
@@ -56,6 +94,23 @@ impl std::fmt::Display for RelativeLocation{
         write!(f, "{}", value)
     }
 }
+
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+pub enum SubscribeResult {
+    Success,
+    AlreadySubscribed,
+    NotSubscribed,
+    UpdatedSubscription,
+    Failed,
+}
+
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+pub enum RegisterResult{
+    Success,
+    AlreadyRegistered,
+    Failed,
+}
+
 
 #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
 pub enum CloseConnectionReason{
