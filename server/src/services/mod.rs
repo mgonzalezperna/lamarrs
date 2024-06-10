4a554c51b582e0cf9cd328bbcfe4d88d23390888
Martin Gonzalez Perna <mgonzalezperna@gmail.com>
2024-06-10 23:14:59 +0200

    add new messages to library

--- server/src/services/mod.rs
+++ server/src/services/mod.rs
@@ -1,2 +1,29 @@
-pub mod text;
-pub use text::TextStreamer as Text;
+pub mod text_streamers;
+
+pub mod payload {
+    use lamarrs_utils::{
+        enums::{Color, GatewayMessage, RelativeLocation},
+        messages::Subtitle,
+    };
+    use tokio::sync::mpsc::Sender;
+    use uuid::Uuid;
+
+    #[derive(Clone, Debug)]
+    pub struct SusbcriptionData {
+        pub sender_id: Uuid,
+        pub sender: Sender<GatewayMessage>,
+        pub location: RelativeLocation,
+    }
+
+    #[derive(Clone, Debug)]
+    pub struct SendSubtitle {
+        pub subtitle: Subtitle,
+        pub target_location: RelativeLocation,
+    }
+
+    #[derive(Clone, Debug)]
+    pub struct SendColor {
+        pub color: Color,
+        pub target_location: RelativeLocation,
+    }
+}
