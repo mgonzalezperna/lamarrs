8a9357e9729f1666d129f2fe53a876f00ae1c814
Tincho <31832018+mgonzalezperna@users.noreply.github.com>
2024-06-22 10:51:42 +0200

    Spike research dioxus to create a UI prototype
    The original client uses tokio, which doesn't play nice with WASM.
    I had to write a new client. This is still a WIP, I may move it out of this module. Not sure yet.
    
    * add new client for web-ui
    * main app, making use of the new websocket client
    * remove tokio deps from lamarrs-utils, to avoid making dioxus go kaput
    * remove legacy function from utils
    * update general Cargo file, add new workspace

---
+++ web-ui/src/main.rs
@@ -0,0 +1,43 @@
+#![allow(non_snake_case)]
+
+use std::{fmt::format, future::IntoFuture};
+pub mod websocket;
+
+use dioxus::prelude::*;
+use futures::pin_mut;
+use futures_util::{future, SinkExt, StreamExt, TryStreamExt};
+use lamarrs_utils::enums::{RelativeLocation, Service, SubscriberMessage};
+use uuid::{serde, Uuid};
+use log::{info, debug, error};
+
+fn main() {
+    // Init logger
+    wasm_logger::init(wasm_logger::Config::new(log::Level::Debug));
+    launch(App);
+}
+
+#[component]
+fn App() -> Element {
+    let uuid = Uuid::new_v4();
+    let location = RelativeLocation::Center;
+    let background_color = use_signal(|| String::from("red"));
+    let subtitle = use_signal(|| String::from(""));
+    let ws: Coroutine<SubscriberMessage> = use_coroutine(|mut rx: UnboundedReceiver<SubscriberMessage>| async move {
+        let mut conn = websocket::WebsocketService::new(background_color, subtitle);
+        let register = conn.sender.try_send(SubscriberMessage::Register((uuid, location.clone())));
+        debug!("Register results {:?}", register);
+        loop {
+            while let Some(message) = rx.next().await {
+                conn.sender.try_send(message);
+            }
+        };
+    });
+
+    rsx! {
+       div {
+            style: "left: 0; top: 0; position: fixed; width: 100%; height: 100%; background-color: { background_color }",
+            id: "background",
+            button { onclick: move |_| ws.send(SubscriberMessage::Subscribe(Service::Color)), "Subscribe to Color" }
+       }
+    }
+}
---
+++ web-ui/src/websocket.rs
@@ -0,0 +1,61 @@
+use dioxus::signals::{Signal, Writable};
+use futures::{channel::mpsc::{Receiver, Sender}, SinkExt, StreamExt};
+use lamarrs_utils::enums::{GatewayError, GatewayMessage, SubscriberMessage};
+use reqwasm::websocket::{futures::WebSocket, Message};
+
+use wasm_bindgen_futures::spawn_local;
+
+pub struct WebsocketService {
+    pub sender: Sender<SubscriberMessage>,
+}
+
+impl WebsocketService {
+    pub fn new(mut bg: Signal<String>, mut subs: Signal<String>) -> Self {
+        let ws = WebSocket::open("ws://127.0.0.1:8080").unwrap();
+
+        let (mut outgoing, mut incoming) = ws.split();
+
+        let (sender, mut receiver) = futures::channel::mpsc::channel::<SubscriberMessage>(32);
+
+        spawn_local(async move {
+            while let Some(subscriber_message) = receiver.next().await {
+                log::debug!("Sending message to Gateway {}", subscriber_message);
+                outgoing.send(Message::Text(serde_json::to_string(&subscriber_message).unwrap())).await.unwrap();
+            }
+        });
+
+        spawn_local(async move {
+            log::info!("Waiting for Gateway messages...");
+            loop {
+                while let Some(msg) = incoming.next().await {
+                    log::debug!("Processing new msg");
+                    match msg {
+                        Ok(Message::Text(payload)) => {
+                            log::debug!("From Gateway: {}", payload);
+                            match serde_json::from_str(&payload) {
+                                Ok(GatewayMessage::Color(color)) => {
+                                    log::info!("Request change of Color by Gateway: {}", color);
+                                    bg.set(color.to_string());                                
+                                }
+                                Err(_) => todo!(),
+                                _ => {
+                                    log::info!("Notification Message Received: {}", payload);
+                                },
+                        }},
+                        Err(e) => {
+                            log::error!("ERROR: {:?}", e)
+                        }
+                        _ => {
+                            log::debug!("Weird message received from Gateway")
+                        }
+                    }
+                }
+                log::debug!("WebSocket Closed");
+            }
+        });
+
+        Self { 
+            sender
+        }
+    }
+}
