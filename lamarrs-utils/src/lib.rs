944a0ceeb38abcb30496a18e9145fa828e3c20a4
Martin Gonzalez Perna <mgonzalezperna@gmail.com>
2024-06-22 10:49:29 +0200

    remove legacy function from utils

--- lamarrs-utils/src/lib.rs
+++ lamarrs-utils/src/lib.rs
@@ -1,18 +1,3 @@
 pub mod error;
 pub mod enums;
 pub mod messages;
-
-use tokio::io::AsyncReadExt;
-
-// Our helper method which will read data from stdin and send it along the
-// sender provided.
-pub async fn read_stdin() -> Vec<u8> {
-    let mut stdin = tokio::io::stdin();
-    let mut buf = vec![0; 1024];
-    let n = match stdin.read(&mut buf).await {
-        Err(_) | Ok(0) => panic!(),
-        Ok(n) => n,
-    };
-    buf.truncate(n);
-    buf
-}
