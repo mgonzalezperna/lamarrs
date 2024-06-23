66b595c98f88dee4168775af73bb4a2a7a6e5bd7
Martin Gonzalez Perna <mgonzalezperna@gmail.com>
2024-06-23 20:42:31 +0200

    add FromStr and ToString impl to Subtitles to make them MQTTeables

--- lamarrs-utils/src/messages.rs
+++ lamarrs-utils/src/messages.rs
@@ -1,22 +1,40 @@
 // Collection of messages types to be send via websockets.
 
-use serde::{Deserialize, Serialize};
+use std::{fmt, path::Display, str::FromStr, string::ParseError};
+
 use crate::enums::{Color, RelativeLocation, Service};
 use arrayvec::ArrayString;
+use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Suscribe{
+pub struct Suscribe {
     pub uuid: Uuid,
     pub service: Service,
-    pub location: RelativeLocation
+    pub location: RelativeLocation,
 }
 
 #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Subtitle{
+pub struct Subtitle {
     pub subtitle: ArrayString<35>,
+}
+
+impl FromStr for Subtitle {
+    type Err = ParseError;
+
+    fn from_str(value: &str) -> std::result::Result<Self, std::convert::Infallible> {
+        let mut new_subtitles = Self {
+            subtitle: ArrayString::<35>::new(),
+        };
+        new_subtitles.subtitle.push_str(value);
+        Ok(new_subtitles)
+    }
+}
+
+impl fmt::Display for Subtitle {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.subtitle)
+    }
 }
 
 #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
