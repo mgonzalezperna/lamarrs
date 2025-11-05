e52d60edd2145952c80af54df8ba11e0db8e4f0b
Martin Gonzalez Perna <mgonzalezperna@gmail.com>
2025-11-05 20:48:46 +0100

    rework utils: new client-server messages
    A complete rework of the messages between Server and Clients. These messages are no-std compatible, enabling us to use them in the rp-client too.
    ExchangeMessage wraps Actions to be executed by the receptor, while Data wraps the information that will be required by the receptor to execute the requested action.
    
    Dividing ExchangeMessages between Client msg and Server msg may bring dividens in the future, but by now, we are fine mixing them all.

---
+++ lamarrs-utils/src/action_messages.rs
@@ -0,0 +1,27 @@
+use serde::{Deserialize, Serialize};
+use strum::Display;
+
+use crate::{AudioFile, ClientIdAndLocation, ColourRgb, Service, Subtitles};
+
+/// These are the payloads the clients will be sending inside the Exchange Messages.
+/// In the future, they may be also the payloads between services. Some feature gating
+/// will be required for it.
+#[derive(Deserialize, Serialize, PartialEq, Debug)]
+//#[non_exhaustive]
+pub enum Action {
+    Register(ClientIdAndLocation),
+    SuscribeToService(Service, ClientIdAndLocation),
+    UnsubscribeFromService(Service, ClientIdAndLocation),
+    UpdateLocation(ClientIdAndLocation),
+    UpdateClient(Data),
+}
+
+/// Internal message types to be transmited between actors inside Lamarrs.
+/// These are also the payloads the clients will be sending inside the Exchange Messages.
+#[derive(Deserialize, Serialize, PartialEq, Debug, Display, Clone)]
+pub enum Data {
+    NewSubtitles(Subtitles),
+    ChangeColour(ColourRgb),
+    PlayAudio(AudioFile),
+    //MIDIEvent(MidiEvent), We are dropping MIDI support by now, since this utils must be no_std friendly.
+}
---
+++ lamarrs-utils/src/exchange_messages.rs
@@ -0,0 +1,36 @@
+use serde::{Deserialize, Serialize};
+
+use crate::{action_messages::Action, ErrorDescription};
+
+/// Wrapper for the messages traveling between the Clients and the Server
+///
+/// There are 5 types:
+///  * Ack: Server > Client. Confirmation of requested action and results.
+///  * Nack: Server > Client. Rejection of request and its reason.
+///  * Request: Client > Server. Request sent by the client to the server to perform an action.
+///  * Update: Server > Client. Update sent one of the service the client is subscribed to.
+///  * Error: Server > Client. Jocker type message for yet-unmapped error cases.
+#[derive(Deserialize, Serialize, PartialEq, Debug)]
+pub enum ExchangeMessage {
+    Ack(AckResult),
+    Nack(NackResult),
+    Request(Action),
+    Update(Action),
+    Error(ErrorDescription),
+}
+
+/// Results on the latest request sent by client if succeeds.
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+pub enum AckResult {
+    Success,
+    UpdatedSubscription,
+    UpdatedLocation,
+}
+
+/// Results on the latest request sent by client if fails.
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+pub enum NackResult {
+    AlreadySubscribed,
+    NotSubscribed,
+    Failed,
+}
